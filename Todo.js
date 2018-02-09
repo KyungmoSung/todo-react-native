90f1e398793c102614398c5cd7a8cf79ee95acc4
Sung Kyungmo <skm280@naver.com>
2018-02-09 13:55:46 +0900

    Todo 리스트에 체크박스 이벤트 추가

--- Todo.js
+++ Todo.js
@@ -1,17 +1,70 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions
+} from "react-native";
+
+const { width, height } = Dimensions.get("window");
 
 class Todo extends Component {
-    render() {
-        return (
-            <View>
-                <Text>I am Todo</Text>
-            </View>
-        );
-    }
+  state = {
+    isEditing: false,
+    isCompleted: false
+  };
+  render() {
+    const { isCompleted } = this.state;
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.toggleComplete}>
+          <View
+            style={[
+              styles.circle,
+              isCompleted ? styles.completedCircle : styles.uncompletedCircle
+            ]}
+          />
+        </TouchableOpacity>
+        <Text style={styles.text}>I am Todo</Text>
+      </View>
+    );
+  }
+  toggleComplete = () => {
+    this.setState(prevState => {
+      return {
+        isCompleted: !prevState.isCompleted
+      };
+    });
+  };
 }
-const style = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({
+  container: {
+    width: width - 50,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderColor: "red",
+    borderWidth: 2,
+    marginRight: 20
+  },
+  completedCircle: {
+    borderColor: "#bbb"
+  },
+  uncompletedCircle: {
+    borderColor: "#F23657"
+  },
+  text: {
+    fontWeight: "600",
+    fontSize: 20,
+    marginVertical: 20
+  }
+});
 
 export default Todo;
